6e2a19160afaa07aa9acffedb6cbf4c1e0d5ea5c
slogomaan <shashwatbehera@icloud.com>
2024-09-21 16:52:26 +0530

    version 1.1

---
+++ src/sptxtl/settings.py
@@ -0,0 +1,68 @@
+import os
+from pathlib import Path
+from django.db import  models
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'qvki xkuc hcfn wvjv'
+DEBUG = True
+
+ALLOWED_HOSTS = ['*.vercel.app', '*.now.sh', '127.0.0.1']
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'transcribe',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'sptxtl.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'sptxtl.wsgi.application'
+LANGUAGE_AUTO_FIELD='django.db.models.BigAutoField'
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 # SMTP Port
+EMAIL_USE_TLS = True # Enable TLS
+EMAIL_HOST_USER = 'createshashwhaat@gmail.com' # Your email ID
+EMAIL_HOST_PASSWORD = 'qvki xkuc hcfn wvjv'  # Use environment variables for security
---
+++ src/sptxtl/wsgi.py
@@ -0,0 +1,9 @@
+# wgsi.py
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sptxtl.settings')
+
+application = get_wsgi_application()
